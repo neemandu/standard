a9236c51060fbf1ad430f49573cf253e25b3718e
neemandu <neemandu@gmail.com>
2021-07-12 18:38:09 +0300

    4

--- src/app/marketing/marketing.component.ts
+++ src/app/marketing/marketing.component.ts
@@ -28,19 +28,13 @@ export class MarketingComponent implements OnInit {
       let day = this.dates[i].getDate();
       if(day == 1 || day == 11 || day == 21){
         this.counter++;
-        console.log(this.dates[i]);
       }
     }
-    console.log("this.counter " + this.counter);
-    console.log("this.allCoaches.items: " + this.allCoaches.items[0].coaches.length);
     for(var i =0 ; i< this.allCoaches.items[0].coaches.length; i++){
       let currCoach = this.allCoaches.items[0].coaches[i];
       let otherIndex = i;
       for(var j= 1; j<= this.counter; j++){
         otherIndex++;
-        console.log("i: " + i);
-        console.log("j: " + j);
-        console.log("otherIndex: " + otherIndex);
         if(otherIndex == i){
           otherIndex++;
         }
@@ -50,7 +44,6 @@ export class MarketingComponent implements OnInit {
         if(otherIndex == i){
           otherIndex++;
         }
-        console.log("otherIndex: " + otherIndex);
       }
       this.matches.push(this.allCoaches.items[0].coaches[otherIndex].firstName + " " + this.allCoaches.items[0].coaches[otherIndex].lastName + " משווק את " + currCoach.firstName + " " + currCoach.lastName)
     }
