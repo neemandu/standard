2482ecfa00e51b7825ef29ae4a162765d102f4a5
neemandu <neemandu@gmail.com>
2021-07-13 15:08:00 +0300

    4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,22 @@ import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter.pipe';
+import { FormsModule } from '@angular/forms';
 import { MarketingComponent } from './marketing/marketing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MarketingComponent
+    MarketingComponent, 
+    FilterPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     /* configure app with AmplifyUIAngularModule */
-    AmplifyUIAngularModule
+    AmplifyUIAngularModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/marketing/marketing.component.ts
+++ src/app/marketing/marketing.component.ts
@@ -13,12 +13,13 @@ export class MarketingComponent implements OnInit {
   dates: any[] = [];
   counter = 0;
   matches: any[] = [];
+  searchText = '';
 
   constructor(private api: APIService) { }
 
   ngOnInit(): void {
   this.api.ListStandards().then(standard => {
-    console.log(standard);
+    
     this.allCoaches = standard;
     let now =  new Date();
     let start = new Date(2021, 1, 1, 0, 0, 0);
@@ -27,25 +28,43 @@ export class MarketingComponent implements OnInit {
     for(var i =0 ; i< this.dates.length; i++){
       let day = this.dates[i].getDate();
       if(day == 1 || day == 11 || day == 21){
+        console.log(this.dates[i]);
         this.counter++;
       }
     }
+    console.log(this.counter);
+    for(var i =0 ; i< this.allCoaches.items[0].coaches.length; i++){
+      console.log(this.allCoaches.items[0].coaches[i]);
+    }
     for(var i =0 ; i< this.allCoaches.items[0].coaches.length; i++){
       let currCoach = this.allCoaches.items[0].coaches[i];
+      console.log(currCoach.firstName);
+      console.log("i: " + i);
       let otherIndex = i;
       for(var j= 1; j<= this.counter; j++){
+        console.log('otherIndex: ' + otherIndex);
+        console.log("j: " + j);
         otherIndex++;
         if(otherIndex == i){
+          console.log("otherIndex == i 1");
           otherIndex++;
         }
         if(otherIndex == this.allCoaches.items[0].coaches.length){
+          console.log("otherIndex == this.allCoaches.items[0].coaches.length");
           otherIndex = 0;
         }
         if(otherIndex == i){
+          console.log("otherIndex == i 2");
           otherIndex++;
         }
       }
-      this.matches.push(this.allCoaches.items[0].coaches[otherIndex].firstName + " " + this.allCoaches.items[0].coaches[otherIndex].lastName + " משווק את " + currCoach.firstName + " " + currCoach.lastName)
+      
+      console.log('FINAL otherIndex: ' + otherIndex);
+      this.matches.push(currCoach.firstName + " " + currCoach.lastName + 
+       " משווק/ת את " + 
+       this.allCoaches.items[0].coaches[otherIndex].firstName + " "
+       + this.allCoaches.items[0].coaches[otherIndex].lastName
+       )
     }
     console.log(this.matches);
   });
