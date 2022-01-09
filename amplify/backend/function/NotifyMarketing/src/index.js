d6190b0a2d58ef916625c366fbc9389731b94325
neemandu <neemandu@gmail.com>
2022-01-09 20:44:15 +0200

    fix dates

--- amplify/backend/function/NotifyMarketing/src/index.js
+++ amplify/backend/function/NotifyMarketing/src/index.js
@@ -2,17 +2,25 @@ const aws = require('aws-sdk')
 const ses = new aws.SES()
 
 exports.handler = async (event) => {
-    let start = new Date(2021, 10, 2, 0, 0, 0);
+    let start = new Date(2021, 9, 2, 0, 0, 0);
     let tomorrow  =  new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
 
+    console.log("tomorrow: ");
+    console.log(tomorrow);
+    console.log("start: ");
+    console.log(start);
     var Difference_In_Time = tomorrow.getTime() - start.getTime();
-    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+    console.log("Difference_In_Time: ");
+    console.log(Difference_In_Time);
+    var Difference_In_Days = Math.floor(Difference_In_Time / (1000 * 3600 * 24));
+    console.log("Difference_In_Days: " + Difference_In_Days);
     if(Difference_In_Days % 20 == 0){
+        console.log("should send email!");
         await ses
         .sendEmail({
             Destination: {
-                ToAddresses: [process.env.SES_EMAIL],
+                ToAddresses: ["standard12520@gmail.com"],
             },
             Source: process.env.SES_EMAIL,
             Message: {
